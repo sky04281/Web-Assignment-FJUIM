f1d098804359c6a38c50eb9bb5b6d6a46a8b9a2d
sky04281 <sky04282@gmail.com>
2022-12-30 18:24:21 +0800

    整理檔案 header footer 改 include

---
+++ theme/header.php
@@ -0,0 +1,17 @@
+
+<nav class="navbar navbar-expand-lg py-4 navigation header-padding " id="navbar">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse text-center" id="navbarsExample09">
+            <ul class="navbar-nav m-left">
+                <li class="nav-item active">
+                    <a class="nav-link" href="im_index.php">首頁 <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="im_reservation.php">預約系統</a></li>
+                <li class="nav-item"><a class="nav-link" href="im_calander.php">行事曆</a></li>
+                <li class="nav-item"><a class="nav-link" href="im_announcement.php">最新公告</a></li>
+                <li class="nav-item"><a class="nav-link" href="im_contact.php">關於我們</a></li>
+            </ul>
+        </div>
+        <a href="im_login.php" class="btn btn-solid-border d-none d-lg-block">登入<i class="fa fa-angle-right ml-2"></i></a>
+    </div>
+</nav>
