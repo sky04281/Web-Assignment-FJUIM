174b59e4e9505b3a19c99bcb44a75d8afd768f60
sky04281 <sky04282@gmail.com>
2023-01-06 02:04:46 +0800

    1

--- theme/im_announcement.php
+++ theme/im_announcement.php
@@ -1,12 +1,4 @@
 <!DOCTYPE html>
-
-<!--
- // WEBSITE: https://themefisher.com
- // TWITTER: https://twitter.com/themefisher
- // FACEBOOK: https://www.facebook.com/themefisher
- // GITHUB: https://github.com/themefisher/
--->
-
 <html lang="zxx">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
